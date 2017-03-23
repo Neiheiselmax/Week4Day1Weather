169176f052010738b89273f56d00ccf9f8e61bea
Max Neiheisel <maxneiheisel@Max-Neiheisels-MacBook-2.local>
2017-03-22 21:20:32 -0400

    added weather image

--- JS/main.js
+++ JS/main.js
@@ -2,7 +2,7 @@ $('#zip').submit(function(e){
 	e.preventDefault();
 	var name = $('#input').val();
 	var url ='http://api.openweathermap.org/data/2.5/forecast/daily?q=' + name + "&cnt=7&units=imperial&APPID=f20e670d231db943d685d23aa9f11620"
-	$('#cityInput').val('');
+	$('#input').val('');
 	$('.weatherbox').html('');	
 
 
@@ -12,9 +12,10 @@ $.ajax({url: url,
 	response.list.forEach(function(weather){
 		console.log(weather)
 			$('.weatherbox').append( `
-				<div class="divide col-md-1">
-				<h2>Max. Temp: ${weather.temp.max}</h2>
+				<div class="divide col-md-1 data">
+				<h2 id="max">Max. Temp: ${weather.temp.max}</h2>
 				<h2>Min. Temp: ${weather.temp.min}</h2>
+				<img class="icon animated pulse infinite" src="http://openweathermap.org/img/w/${weather.weather[0].icon}.png"
 				</div>
 				`);
 
